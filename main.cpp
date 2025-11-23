2c11e715eb7dc5ab38d7f25d7e416e485d71e883
Jakub Flaga <jakoflago@gmail.com>
2025-11-23 21:40:44 +0100

    Dodanie do main.cpp wywołań aby zademonstrować sortowanie tablic int i double

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 ﻿
 #include <iostream>
+#include <vector>
+#include "MergeSort.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+template <typename T>
+void printArray(const std::vector<T>& arr) {
+    for (const auto& val : arr) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    
+    std::cout << "--- Test dla int ---" << std::endl;
+    std::vector<int> tablicaInt = { 12, 7, 14, 9, 10, 11 };
+    MergeSort<int> sorterInt;
+
+    std::cout << "Przed: ";
+    printArray(tablicaInt);
+
+    sorterInt.sort(tablicaInt);
+
+    std::cout << "Po:    ";
+    printArray(tablicaInt);
+
+    
+    std::cout << "\n--- Test dla double ---" << std::endl;
+    std::vector<double> tablicaDouble = { 3.14, 1.59, 2.65, 5.35, 0.11 };
+    MergeSort<double> sorterDouble;
+
+    std::cout << "Przed: ";
+    printArray(tablicaDouble);
+
+    sorterDouble.sort(tablicaDouble);
+
+    std::cout << "Po:    ";
+    printArray(tablicaDouble);
+
+    return 0;
 }
